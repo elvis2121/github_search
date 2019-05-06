544d048c77805736d2f79e72f0f496c86710b243
elvis2121 <elvisnjau@gmail.com>
2019-05-06 17:56:40 +0300

    access token moved to user service

--- src/app/users.service.ts
+++ src/app/users.service.ts
@@ -1,18 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {environment} from '../environments/environment'
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
   apiUrl = "https://api.github.com/users/"
-  username: string; 
+  username: string;
+  api_key ='?access_token=cae8139569a21bb8edfd467a4072340ea3330013'
 
   constructor(private http: HttpClient) { }
 
   getUsers(){
-    return this.http.get(this.apiUrl+this.username+environment.api_key)
+    return this.http.get(this.apiUrl+this.username+this.api_key)
   }
 
   updateUser(info){
