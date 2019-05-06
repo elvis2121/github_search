785e4ed86b9844c55a7eb10af9a44c7f7efc8c3d
elvis2121 <elvisnjau@gmail.com>
2019-05-06 17:39:08 +0300

    accesstoken replacement

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  api_key:'?access_token=082fcd35cd2e5888dade18063b55fe1442d5df1a'
+  api_key:'?access_token=e1af1ca7d36a768d5efc235f8ff504ea5a00f97b'
 };
